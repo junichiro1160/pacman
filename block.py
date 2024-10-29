bed72966b6c099ebd70263f3ed04da674f7bf5ee
Yuto <s243140@stn.nagaokaut.ac.jp>
2024-10-29 17:38:03 +0900

    ブロック作ったよ

---
+++ block.py
@@ -0,0 +1,34 @@
+from item import Item
+import doctest
+
+
+class Block(Item):
+    """
+    Blockクラス
+    アイテムの位置とアイコンを管理するクラス
+
+    Attributes:
+        x (int): x座標
+        y (int): y座標
+        icon (str): 表示アイコン
+
+    Examples:
+        >>> block = Block(3, 3)
+        >>> block.now_x
+        3
+        >>> block.now_y
+        3
+        >>> block.icon
+        '🌴'
+        >>> isinstance(block, Item)
+        True
+    """
+    pass
+
+    def __init__(self, x, y) -> None:
+        super().__init__(x, y)
+        self.icon = "🌴"
+
+
+if __name__ == "__main__":
+    doctest.testmod()
--- field.py
+++ field.py
@@ -28,11 +28,9 @@ class Field:
             foods: list[Food],
             blocks: list[Block],
             f_size: int = 6) -> None:
-        pass
 
         """
         Fieldクラスの初期化を行う関数
-        
         Args:
             players (list[Player]): プレイヤーのリスト
             enemies (list[Enemy]): 敵のリスト
@@ -40,16 +38,15 @@ class Field:
             blocks (list[Block]): ブロックのリスト
             f_size (int): フィールドのサイズ
         """
+        pass
 
-
-     def update_field(self) -> list[list[str]]:
-     """
-     敵、プレイヤー、アイテムを配置を参照して、Fieldを更新する関数
-     
-     Returns
+    def update_field(self) -> list[list[str]]:
+        """
+        敵、プレイヤー、アイテムを配置を参照して、Fieldを更新する関数
+        Returns
         list[list[str]]: 更新されたField
-        
-    Examples:
+
+        Examples:
         >>> p = [Player(1,0)]
         >>> p[0].icon = "p1"
         >>> e1 = Enemy(2,0)
@@ -72,63 +69,65 @@ class Field:
         >>> field.update_field()[2]
         ['b1','b2','\\u3000']
         """
-
-     # 衝突判定を行う関数
-     def check_bump( self, target: Item, items: list[Item]) -> Item | None:
         pass
-     
-     """
-     2つのアイテムの位置が重なっているか判定する関数
-     
-     Args:
-        target (Item): アイテム1
-        items (list[Item]): アイテムのリスト2
-    
-    Returns:
-        Item | None: 重なっているアイテムがあればそのアイテム、なければNone
-        
-    Examples:
-        >>> p = Item(0,0)
-        >>> e = Item(1,1)
-        >>> field = Field([p],[e],[],[])
-        >>> p.next_x = 1
-        >>> r = field.check_bump(p,[e])
-        >>> r is None
-        True
-        >>> p.next_y = 1
-        >>> r = field.check_bump(p,[e])
-        >>> r is e
-        True
+
+    # 衝突判定を行う関数
+    def check_bump(self, target: Item, items: list[Item]) -> Item | None:
+
         """
-     
+        2つのアイテムの位置が重なっているか判定する関数
+
+        Args:
+            target (Item): アイテム1
+            items (list[Item]): アイテムのリスト2
+
+        Returns:
+            Item | None: 重なっているアイテムがあればそのアイテム、なければNone
+
+        Examples:
+            >>> p = Item(0,0)
+            >>> e = Item(1,1)
+            >>> field = Field([p],[e],[],[])
+            >>> p.next_x = 1
+            >>> r = field.check_bump(p,[e])
+            >>> r is None
+            True
+            >>> p.next_y = 1
+            >>> r = field.check_bump(p,[e])
+            >>> r is e
+            True
+        """
+    pass
+
     # Fieldを表示する関数
     def display_field(self) -> None:
-       pass
-    """
-    Fieldを表示する関数
-    
-    Examples:
-        >>> p = [Player(1,0)]
-        >>> p[0].icon = "p1"
-        >>> e1 = Enemy(2,0)
-        >>> e1.icon = "e1"
-        >>> e2 = Enemy(1,1)
-        >>> e2.icon = "e2"
-        >>> e = [e1,e2]
-        >>> f = [food(0,1)]
-        >>> f[0].icon = "f1"
-        >>> b1 = Block(0,2)
-        >>> b1.icon = "b1"
-        >>> b2 = Enemy(1,2)
-        >>> b2.icon = "b2"
-        >>> b = [b1,b2]
-        >>> field = Field(p,e,f,b,3)
-        >>> field.display_field()
-        w: 上に移動
-        a: 左に移動
-        s: 下に移動
-        d: 右に移動
-          p1e1
-        f1e2
-        b1b2
+
         """
+        Fieldを表示する関数
+
+        Examples:
+            >>> p = [Player(1,0)]
+            >>> p[0].icon = "p1"
+            >>> e1 = Enemy(2,0)
+            >>> e1.icon = "e1"
+            >>> e2 = Enemy(1,1)
+            >>> e2.icon = "e2"
+            >>> e = [e1,e2]
+            >>> f = [food(0,1)]
+            >>> f[0].icon = "f1"
+            >>> b1 = Block(0,2)
+            >>> b1.icon = "b1"
+            >>> b2 = Enemy(1,2)
+            >>> b2.icon = "b2"
+            >>> b = [b1,b2]
+            >>> field = Field(p,e,f,b,3)
+            >>> field.display_field()
+            w: 上に移動
+            a: 左に移動
+            s: 下に移動
+            d: 右に移動
+            p1e1
+            f1e2
+            b1b2
+            """
+        pass
