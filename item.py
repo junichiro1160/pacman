5540829d081717f4750061ef623f0f5341f85fd2
甲斐純一郎 <jun1160.k@gmail.com>
2024-11-12 17:10:48 +0900

    item.py

--- item.py
+++ item.py
@@ -16,4 +16,100 @@ class Item:
     """
 
     def __init__(self, x, y) -> None:
+        """
+        Itemクラスのコンストラクタ
+        引数にx座標とy座標を受け取り、それぞれの座標を初期化する.
+
+        Args:
+            x(int): x座標
+            y(int): x座標
+
+        Returns:
+            None
+
+        Examples:
+            >>> item = Item(2, 3)
+            >>> item.now_x
+            2
+            >>> item.now_y
+            3
+            >>> item.next_x
+            2
+            >>> item.next_y
+            3
+            >>> item.icon
+            ''
+            >>> item.status
+            True
+        """
+        self.now_x = x
+        self.now_y = y
+        self.next_x = x
+        self.next_y = y
+        self.status = True
+        self.icon = ""
         pass
+
+    def get_next_pos(self) -> tuple[int, int]:
+        """
+        次の時刻の座標を取得するメソッド
+        デフォルトでは現在の座標を返すため、子クラスでオーバーライドすることを設定している.
+
+        Returns:
+            tuple[int, int]:現在の座標
+
+        Examples:
+            >>> item = Item(2,3)
+            >>> item.get_next_pos()
+            (2,3)
+        """
+        return (self.now_x, self.now_y)
+
+    def get_pos(self) -> tuple[int, int]:
+        """
+        次の時刻の座標を取得するメソッド
+        デフォルトでは現在の座標を返すため、子クラスでオーバーライドすることを想定している.
+
+        Returns:
+            tuple[int, int]:現在の座標
+
+        Example:
+            >>> item = Item(2,3)
+            >>> item.get_pos()
+            (2,3)
+        """
+        return (self.now_x, self.now_y)
+
+    def update_pos(self, stuck: bool = False) -> None:
+        """
+        座標を更新するメソッド
+        引数に次に移動したい座標を取り、その座標にプレイヤーの現在の座標を更新する.
+
+        Args:
+            stuck (bool): そのターンに動けない場合にTrueを渡す (defoult: False)
+
+        Returns:
+            None
+
+        Examples:
+            >>> item = Item(2,3)
+            >>> item.next_x = 3
+            >>> item.next_y = 4
+            >>> item.get_pos()
+            (2,3)
+            >>> item.update_pos()
+            >>> item.get_pos()
+            (3,4)
+
+        """
+        if stuck:
+            self.next_x = self.now_x
+            self.next_y = self.now_y
+            return
+        self.now_x = self.next_x
+        self.now_y = self.next_y
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
